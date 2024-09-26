8af4279eb7ffd47a3225fef758962fa3d8407ccd
Slava <slava@reddingdesigns.com>
2024-09-26 16:12:22 -0400

    initial commit

---
+++ wp-content/themes/rd-theme/blocks/black-banner/render.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ABlack Banner Block Template.
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param bool $is_preview True during backend preview render.
+ * @param int $post_id The post ID the block is rendering content against.
+ *        This is either the post ID currently being displayed inside a query loop,
+ *        or the post ID of the post hosting this block.
+ * @param array $context The context provided to the block by the post or it's parent block.
+ * 
+ * @package rd-theme
+ */
+?>
+
+<section>
+    <div class="container bg-off-black">
+      <div class="flex justify-between">
+        <div class="flex flex-col items-center">
+          <p class="text-green">Discover all our</p>
+          <p class="text-green">100% electric products</p>
+        </div>
+      </div>
+    </div>
+</section>
