4bc303b5d2e0a84332a189991d52f35fe50aafdb
Slava <slava@reddingdesigns.com>
2024-09-27 11:54:03 -0400

    front page

---
+++ wp-content/themes/rd-theme/page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/header'); ?>
+        <main class="content">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+                    the_content();
+                endwhile;
+            endif;
+            ?>
+        </main>
+    </div>
+<?php get_template_part('template-parts/footer'); ?>
+
+<?php get_footer(); ?>
