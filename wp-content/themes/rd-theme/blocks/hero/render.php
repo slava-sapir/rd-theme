e45b81a73393e0eaab84661f80790949d70d2c28
Jem <jem@reddingdesigns.com>
2024-09-27 14:07:02 -0400

    hero

--- wp-content/themes/rd-theme/blocks/hero/render.php
+++ wp-content/themes/rd-theme/blocks/hero/render.php
@@ -12,15 +12,19 @@
 * @package Tenax_North_America
 */
 $image = get_field('image');
-$video = get_field('video'); ?>
+$video = get_field('video'); 
+$bg = get_field('background_type'); ?>
 
-<section class="h-96 md:h-[805px] bg-off-white">
-    <video autoplay muted loop playsinline>
-        <source src="<?= $video['url']; ?>" type="video/mp4">
-        Your browser does not support the video tag.
-        <?= $video['url']; ?>
-    </video>
-    <div class="container">
+<section class="h-72 sm:h-96 lg:h-[550px] xl:h-[805px] relative" 
+    style="<?= $bg === 'Image' ? "background-image: url('{$image['url']}');" : ''; ?>">
+    <?php if ($bg === 'Video'): ?>
+        <video class="h-72 sm:h-96 lg:h-[550px] xl:h-[805px] z-[1] absolute top-0 left-0 object-cover w-full" 
+            src="<?= $video['url']; ?>" type="video/mp4" autoplay muted loop playsinline>
+            Your browser does not support the video tag.
+            <?= $video['url']; ?>
+        </video>
+    <?php endif; ?>
+    <div class="container text-white relative z-[2] flex content-end h-full flex-wrap pb-[50px] white">
         <InnerBlocks/>
     </div>
 </section>
