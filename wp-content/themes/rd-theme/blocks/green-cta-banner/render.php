8d21f1aa34f5ed27ec11eddc97873de82975ced0
Jem <jem@reddingdesigns.com>
2024-09-30 14:43:25 -0400

    green cta

---
+++ wp-content/themes/rd-theme/blocks/green-cta-banner/render.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Green CTA Banner
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param bool $is_preview True during backend preview render.
+ * @param int $post_id The post ID the block is rendering content against.
+ *        This is either the post ID currently being displayed inside a query loop,
+ *        or the post ID of the post hosting this block.
+ * @param array $context The context provided to the block by the post or it's parent block.
+ * 
+ * @package rd-theme
+ */
+
+ $paddingTop = get_field('padding_top');
+ $paddingBottom = get_field('padding_bottom');
+ $container = get_field('container_width');
+ $bg = get_field('background');
+?>
+
+<section>
+    
+</section>
+
