f7ddb16c549948789b58d07a54890d0e01a274b7
Slava <slava@reddingdesigns.com>
2024-09-30 16:01:06 -0400

    adding background

--- wp-content/themes/rd-theme/resources/js/counter.ts
+++ wp-content/themes/rd-theme/resources/js/counter.ts
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Select all elements with the class 'counter'
+
   const counters: NodeListOf<Element> = document.querySelectorAll(".counter");
 
   counters.forEach((counter: Element) => {
     const targetAttr: string | null = counter.getAttribute("data-target");
-    const target: number = targetAttr ? parseInt(targetAttr) : 0; // Safely handle null
+    const target: number = targetAttr ? parseInt(targetAttr) : 0; 
     let count: number = 0;
     let started: boolean = false;
     const increment: number = target / 100;
@@ -12,14 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateCounter(): void {
       count += increment;
       if (count < target) {
-        counter.textContent = Math.ceil(count).toString(); // Update counter with rounded number
-        setTimeout(updateCounter, 20); // Adjust the speed
+        counter.textContent = Math.ceil(count).toString();
+        setTimeout(updateCounter, 20);
       } else {
-        counter.textContent = target.toString(); // Set the counter to the final number
+        counter.textContent = target.toString();
       }
     }
 
-    // Add hover event listener for each counter
     counter.addEventListener("mouseover", function () {
       if (!started) {
         started = true;
@@ -27,4 +26,5 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+  
 });
