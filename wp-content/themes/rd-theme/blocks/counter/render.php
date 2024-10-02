6e7e85203b2f095e2b0eea4bd1cdd71a68af82fc
Slava <slava@reddingdesigns.com>
2024-10-02 14:45:43 -0400

    responsive

--- wp-content/themes/rd-theme/blocks/counter/render.php
+++ wp-content/themes/rd-theme/blocks/counter/render.php
@@ -15,17 +15,17 @@
 ?>
 
 <section class="pt-[50px] flex justify-center items-center bg-gradient-to-b from-off-black to-white from-[49%] to-[49%] relative">
-  <div class="flex flex-wrap gap-[100px] z-50">
+  <div class="flex gap-[10px] md:gap-[20px] lg:gap-[100px]">
         <?php if (have_rows('counter')) : ?>
           <?php while (have_rows('counter')) : the_row(); 
           $counter_number = get_sub_field('counter_number');
           $counter_title = get_sub_field('counter_title');
           ?>
-          <div class="flex flex-col gap-5">
-            <div class="counter bg-white flex items-center justify-center text-green text-3xl font-medium shadow-md rounded-full w-[180px] h-[180px]" data-target="<?php echo esc_attr($counter_number); ?>">
+          <div class="flex flex-col justify-center items-center gap-5">
+            <div class="counter bg-white flex items-center justify-center text-green text-2xl lg:text-3xl font-medium shadow-md rounded-full w-[60px] h-[60px] md:w-[90px] md:h-[90px] lg:w-[180px] lg:h-[180px]" data-target="<?php echo esc_attr($counter_number); ?>">
                 0
             </div>
-            <p class="text-center text-off-black text-xl font-medium"><?= $counter_title; ?></p>
+            <p class="text-center text-off-black text-sm md:text-xl font-medium"><?= $counter_title; ?></p>
           </div>
         <?php endwhile; endif; ?>
   </div>
