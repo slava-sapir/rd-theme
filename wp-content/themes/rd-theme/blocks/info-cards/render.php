33a32d2bccabba6c3f65bdfb3a752cd99b5618af
Jem <jem@reddingdesigns.com>
2024-10-08 11:46:35 -0400

    drop shadow

--- wp-content/themes/rd-theme/blocks/info-cards/render.php
+++ wp-content/themes/rd-theme/blocks/info-cards/render.php
@@ -16,9 +16,9 @@
 ?>
 <?php if (have_rows('information')): ?>
     <?php while (have_rows('information')): the_row(); 
-    $title = get_sub_field('title');
+    $title = get_sub_field('title'); 
     $link = get_sub_field('link'); ?>
-        <section class="bg-white p-[50px] drop-shadow-[0_0_20px_0_rgba(0,0,0,0.09)] mb-[20px] last:mb-0">
+        <section class="bg-white p-[50px] drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] mb-[30px] last:mb-0">
             <h5 class="font-semibold pb-[25px]"><?= $title ?></h5>
             <a class="text-grey" href="<?= $link['url']; ?>"><?= $link['title']; ?></a>
         </section>
