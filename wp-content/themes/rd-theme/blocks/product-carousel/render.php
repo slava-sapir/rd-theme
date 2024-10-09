b8a3588faaeac2b81e579695a7548b8ae3746a20
Dwayne <dwayne@reddingdesigns.com>
2024-10-09 17:11:38 -0400

    add product carousel block

---
+++ wp-content/themes/rd-theme/blocks/product-carousel/render.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Product Carousel Block Template.
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param bool $is_preview True during backend preview render.
+ * @param int $post_id The post ID the block is rendering content against.
+ *        This is either the post ID currently being displayed inside a query loop,
+ *        or the post ID of the post hosting this block.
+ * @param array $context The context provided to the block by the post or it's parent block.
+ * 
+ * @package rd-theme
+ */
+?>
+
+<?php
+$anchor = isset($block['anchor']) ? $block['anchor'] : '';
+$classes = isset($block['className']) ? $block['className'] : '';
+$args = [
+    'post_type' => 'product',
+];
+$products = new WP_Query($args);
+?>
+
+<section id="<?= $anchor; ?>" class="<?= $classes; ?>" style="padding-top: <?= get_field('padding_top'); ?>px; padding-bottom: <?= get_field('padding_bottom'); ?>px;">
+    <div class="swiper-container product-carousel overflow-x-hidden">
+        <div class="swiper-wrapper">
+            <?php while($products->have_posts()) : $products->the_post(); ?>
+            <div class="swiper-slide relative">
+                <?= wp_get_attachment_image(get_field('accordion_image', get_the_ID())['id'], 'full'); ?>
+                    <h3 class="text-h4 text-off-black font-semibold pt-5 text-center"><?php the_title(); ?></h3>
+                <?php if(get_field('accordion_alt_title', get_the_ID())): ?>
+                    <p class="pt-2 text-center"><?= get_field('accordion_alt_title', get_the_ID()) ?></p>
+                <?php endif; ?>
+                <a href="<?= get_the_permalink(); ?>" title="<?php the_title(); ?>" class="absolute inset-0 z-10"></a>
+            </div>
+            <?php endwhile; ?>
+        </div>
+
+        <div class="swiper-pagination !static pt-12 lg:pt-[80px]"></div>
+    </div>
+
+</section>
